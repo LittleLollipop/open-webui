a0c1ab47cf5fd73163ac314a63df66ba09f5f821
Sai <kldtks@live.com>
2024-09-13 14:39:37 +0800

    fix

--- src/lib/apis/ollama/index.ts
+++ src/lib/apis/ollama/index.ts
@@ -294,7 +294,27 @@ export const generateTextCompletion = async (token: string = '', model: string, 
 	return res;
 };
 
-export const generateChatCompletion = async (token: string = '', body: object) => {
+export const generateChatCompletion = (token: string = '', body: object) => new Promise<[Response, AbortController]>(async (resolve, reject) => {
+	let controller = new AbortController();
+
+	fetch(`${OLLAMA_API_BASE_URL}/api/chat`, {
+		signal: controller.signal,
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${token}`
+		},
+		body: JSON.stringify(body)
+	}).then(resp => {
+		resolve(resp)
+	}) .catch((err) => {
+		reject(err)
+	});
+
+})
+
+export const generateChatCompletion_old = async (token: string = '', body: object) => {
 	let controller = new AbortController();
 	let error = null;
 
